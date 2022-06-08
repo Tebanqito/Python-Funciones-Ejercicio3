0fa040bef3795e9b5e61cbc7ec840766ecb0c039
Tebanqito <yemesesteban@gmail.com>
2022-06-07 21:04:20 -0500

    Primer commit

---
+++ Ej3.py
@@ -0,0 +1,9 @@
+def esBisiesto(anio):
+    if anio % 400 == 0:
+        return True
+    elif anio % 100 == 0:
+        return False
+    elif anio % 4 == 0:
+        return True
+    else:
+        return False
